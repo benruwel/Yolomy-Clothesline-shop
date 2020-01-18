52a32027a64934e9adbece354634402cd025446a
Ben Ruwel <benruwel21@gmail.com>
2020-01-18 19:30:47 +0300

    tweaked the buttons

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
-      $('.scroll-down').on('click', function(e) {
+      $('.scroll-down, #products').on('click', function(e) {
         $('html, body, #hero').animate({ scrollTop: $('#scroll-to').offset().top}, 500, 'linear');
       });
     }
 );
+$(document).ready(function(){
+    $('#women').hide();
+})
 
 $(document).ready(function(){
+  $('#men-hide').attr('checked', true).show();
   $("#men-hide").on('click', function(){
-    
     $("#men").toggle(1000);
   });
 });
 
-$(document).ready(function(){
-    $('#men').hide();
-    $('#women').hide();
-})
+
 
 $(document).ready(function(){
   $("#women-hide").click(function(){
@@ -27,8 +27,3 @@ $(document).ready(function(){
   });
 });
 
-$(document).ready(function(){
-  $("#show").click(function(){
-    $("#men, #women").show(1000);
-  });
-});
