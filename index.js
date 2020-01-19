474f7005303039a0a07c7b9e339276f367d0558e
Ben Ruwel <benruwel21@gmail.com>
2020-01-19 14:10:27 +0300

    revised the UI

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 $(document).ready(function() {
-      $('.scroll-down, #products').on('click', function(e) {
+      $('.scroll-down, #products').on('click', function() {
         $('html, body, #hero').animate({ scrollTop: $('#scroll-to').offset().top}, 500, 'linear');
       });
-    }
-);
+    });
+
 $(document).ready(function(){
     $('#women').hide();
-})
+});
+
+$(document).ready(function(){
+    $('#show').on('click', function(){
+      $('#men, #women').show(1000);
+      $('#men-hide, #women-hide').attr('checked', true);
+    });
+});
 
 $(document).ready(function(){
   $('#men-hide').attr('checked', true).show();
@@ -19,7 +26,6 @@ $(document).ready(function(){
 
 $(document).ready(function(){
   $("#women-hide").click(function(){
-
     $("#women").toggle(1000, function(){
       $('html, body, #men').animate({ scrollTop: $('#women').offset().top}, 500, 'linear');
     });
